9972a79a9017b753d4bb0f8630b0eee5634c29da
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2018-01-29 19:39:01 +0000

    adding user DAO

--- app/src/main/java/phg/com/automotiveoctoengine/daos/UserDAO.java
+++ app/src/main/java/phg/com/automotiveoctoengine/daos/UserDAO.java
@@ -1,8 +1,37 @@
-package phg.com.automotiveoctoengine.dao;
+package phg.com.automotiveoctoengine.daos;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import phg.com.automotiveoctoengine.models.User;
 
 /**
  * Created by phg on 28/01/18.
  */
 
 public class UserDAO {
+
+    // dummy user account list
+    private List<User> userList = new ArrayList<>();
+
+    public boolean register(User user) {
+        // ToDo: use HTTPController to make request to API to save user
+
+        userList.add(user);
+        return true;
+    }
+
+    public User login(User protoUser) {
+        // ToDo: use HTTPController to check username and password match and exist in DB
+        // dummy method to log user in
+        for (User u : userList) {
+            if (u.getUsername().equals(protoUser.getUsername()) &&
+                    u.getPassword().equals(protoUser.getPassword())) {
+                return u;
+            }
+        }
+        return null;
+    }
 }
