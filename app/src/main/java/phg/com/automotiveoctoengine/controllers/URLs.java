b930b6ba9a8fec263d2d9e9104a99a2409abb119
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2018-01-29 19:40:11 +0000

    adding controller classes for shared preferences and URLs

--- app/src/main/java/phg/com/automotiveoctoengine/controllers/URLs.java
+++ app/src/main/java/phg/com/automotiveoctoengine/controllers/URLs.java
@@ -5,4 +5,10 @@ package phg.com.automotiveoctoengine.controllers;
  */
 
 public class URLs {
+
+    private static final String ROOT_URL = "http://192.168.101.1/api/v0.1/";
+
+    public static final String URL_USERS = ROOT_URL + "users";
+    public static final String URL_CLASSIFIER = ROOT_URL + "classify";
+
 }
