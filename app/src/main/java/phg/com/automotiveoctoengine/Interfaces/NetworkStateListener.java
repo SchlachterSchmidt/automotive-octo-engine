ce44297b19be29b5fd3b4072ef69e5220b79618f
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2018-02-11 14:22:43 +0000

    Implemented NetworkStateListener interface and functionality

--- app/src/main/java/phg/com/automotiveoctoengine/Interfaces/NetworkStateListener.java
+++ app/src/main/java/phg/com/automotiveoctoengine/Interfaces/NetworkStateListener.java
@@ -1,8 +1,8 @@
 package phg.com.automotiveoctoengine.Interfaces;
 
-/**
- * Created by pgieschen on 11/02/2018.
- */
+public interface NetworkStateListener {
 
-public class NetworkStateListener {
+    void onNetworkAvailable();
+    void onNetworkUnavailable();
+
 }
